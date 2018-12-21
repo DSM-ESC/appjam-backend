5f340ec2240a63cf0a5a480c93f4281e6be8751c
hwangseonu <hwangseonu12@naver.com>
2018-12-21 14:48:42 +0900

    feat: Add current user

---
+++ src/main/java/me/mocha/appjam/config/WebConfig.java
@@ -0,0 +1,26 @@
+package me.mocha.appjam.config;
+
+import me.mocha.appjam.resolver.UserArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    private final UserArgumentResolver userArgumentResolver;
+
+    public WebConfig(UserArgumentResolver userArgumentResolver) {
+        this.userArgumentResolver = userArgumentResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(userArgumentResolver);
+    }
+
+}
